796575b6793df0642a48aacf18678f232c1dfe69
Amelsaaa <amandamelda76@gmail.com>
2025-04-29 13:20:33 +0700

    Add files via upload

---
+++ Try UTS Pengembangan Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = 9898;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/users", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
